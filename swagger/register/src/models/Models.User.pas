8b9dc6f6cc16bd5b11b342e89609769a24a433bf
ramon-dacioli <ramon_muz@hotmail.com>
2025-07-20 19:51:50 -0300

    atualizando o prenchimento dos Attributes no model

--- swagger/register/src/models/Models.User.pas
+++ swagger/register/src/models/Models.User.pas
@@ -12,12 +12,12 @@ type
     Fid: Int64;
     FlastUpdate: TDateTime;
   public
-    [SwagProp('Código', True)]
+    [SwagProp('Código', '', True, False)]
     property id: Int64 read Fid write Fid;
-    [SwagProp('Nome', True)]
+    [SwagProp('Nome', '', True, False)]
     property name: String read Fname write Fname;
-    [SwagString(100)]
-    [SwagProp('Sobrenome', False)]
+    [SwagString(100, 3)]
+    [SwagProp('Sobrenome', '', True, False)]
     property lastName: String read FlastName write FlastName;
     [SwagIgnore]
     property lastUpdate: TDateTime read FlastUpdate write FlastUpdate;
